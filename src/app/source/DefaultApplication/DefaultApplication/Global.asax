b3ff1f187ff142c2932560f58ab5108938c8508f
MAHMOUD Khaled <S635091@siege.axa-fr.intraxa>
2017-12-26 14:37:25 +0100

    ajout source asp.net

---
+++ src/app/source/DefaultApplication/DefaultApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DefaultApplication.Global" Language="C#" %>
---
+++ src/app/source/DefaultApplication/DefaultApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DefaultApplication.Startup))]
+namespace DefaultApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
